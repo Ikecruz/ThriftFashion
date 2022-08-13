b33095ffb22dffcf08b43c3756834e7bf2cb657c
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2022-08-13 12:08:41 +0100

    update

--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -6,8 +6,14 @@ def index():
         loggedIn = True
     return render_template("index.html", loggedIn=loggedIn)
 
+def about():
+    loggedIn = False
+    if "key" in session:
+        loggedIn = True
+    return render_template("about_us.html", loggedIn=loggedIn)
+
 def contact():
     loggedIn = False
     if "key" in session:
         loggedIn = True
-    return render_template("contact.html", loggedIn=loggedIn)
+    return render_template("contact_us.html", loggedIn=loggedIn)
